f6dbd5ee25db853bc3b93c5ad8937a91be49f291
Alex Jonker <alex.jonker2008@gmail.com>
2026-05-20 02:58:33 +0200

    Fix more stuff

--- src/storage/boot.sh
+++ src/storage/boot.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # This script is executed by systemd at boot time.
+# This script gets run as root.
 
 source /storage/settings.conf
 export SLINT_BACKEND
 
 if [ "$ENABLE_DASH" = "true" ]; then
+    systemctl mask getty@tty1.service
     exec /usr/local/bin/rusty-dash # SLINT_BACKEND is set in settings.conf
 else
-    exec /sbin/agetty --autologin driver --noclear tty1 linux
+    systemctl unmask getty@tty1.service
+    systemctl start getty@tty1.service
 fi
