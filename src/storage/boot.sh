a18c5ba10f8e578a8cb3748062a60ec1a1584f1f
Alex Jonker <alex.jonker2008@gmail.com>
2026-05-20 22:55:01 +0200

    Improve getty fix

--- src/storage/boot.sh
+++ src/storage/boot.sh
@@ -6,9 +6,7 @@ source /storage/settings.conf
 export SLINT_BACKEND
 
 if [ "$ENABLE_DASH" = "true" ]; then
-    systemctl mask getty@tty1.service
     exec /usr/local/bin/rusty-dash # SLINT_BACKEND is set in settings.conf
 else
-    systemctl unmask getty@tty1.service
     systemctl start getty@tty1.service
 fi
