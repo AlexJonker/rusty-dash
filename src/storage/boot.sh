ecc3f4f892a7d0eba659b553d6253efa0c75abee
Alex Jonker <alex.jonker2008@gmail.com>
2026-05-26 16:08:13 +0200

    Fix pi small screen overflow with configurable scale

--- src/storage/boot.sh
+++ src/storage/boot.sh
@@ -4,9 +4,10 @@
 
 source /storage/settings.conf
 export SLINT_BACKEND
+export SLINT_SCALE_FACTOR
 
 if [ "$ENABLE_DASH" = "true" ]; then
-    exec /usr/local/bin/rusty-dash # SLINT_BACKEND is set in settings.conf
+    exec /usr/local/bin/rusty-dash # SLINT_BACKEND and SLINT_SCALE_FACTOR is set in settings.conf
 else
     systemctl start getty@tty1.service
 fi
