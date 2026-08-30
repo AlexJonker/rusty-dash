9bbd548aa62ab2845c15dc2249a8ff39624b4ee8
Alex Jonker <alex.jonker2008@gmail.com>
2026-08-30 14:48:37 +0200

    Fix labwc test script

--- labwc.sh
+++ labwc.sh
@@ -1,1 +1,1 @@
-QS_CONFIG_PATH=~/git/rusty-dash/src/home/alarm/.config/quickshell labwc -C ~/git/rusty-dash/src/home/alarm/.config/labwc
+QS_CONFIG_PATH=./src/home/alarm/.config/quickshell labwc -C ./src/home/alarm/.config/labwc
